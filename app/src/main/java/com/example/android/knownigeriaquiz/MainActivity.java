d1bb33c8bb8fd7d252aeece537a7e97c92f4ebb3
blaze4dem <blaze4dem@gmail.com>
2018-06-29 02:05:34 +0100

    questions increased to 5 from 3

--- app/src/main/java/com/example/android/knownigeriaquiz/MainActivity.java
+++ app/src/main/java/com/example/android/knownigeriaquiz/MainActivity.java
@@ -26,11 +26,10 @@ public class MainActivity extends AppCompatActivity {
     */
 
     LinearLayout intro_view; LinearLayout result_view;
-    LinearLayout question_1_view; LinearLayout question_2_view; LinearLayout question_3_view; //LinearLayout question_4_view;
+    LinearLayout question_1_view; LinearLayout question_2_view; LinearLayout question_3_view; LinearLayout question_4_view;
+    LinearLayout question_5_view;
 
 
-
-//    LinearLayout question_5_view = (LinearLayout) findViewById(R.id.question_5_view);
 //    LinearLayout question_6_view = (LinearLayout) findViewById(R.id.question_6_view);
 //    LinearLayout question_7_view = (LinearLayout) findViewById(R.id.question_7_view);
 //    LinearLayout question_8_view = (LinearLayout) findViewById(R.id.question_8_view);
@@ -38,18 +37,28 @@ public class MainActivity extends AppCompatActivity {
 //    LinearLayout question_10_view = (LinearLayout) findViewById(R.id.question_10_view);
 
 
-
+    /*
+    ............. Introduction page with the edit text for the name... and also setting up all the views.......
+     */
 
     public void startQuiz(View view){
+
+        // update all the views to display........
+
         intro_view = (LinearLayout) findViewById(R.id.intro_view);
         result_view = (LinearLayout) findViewById(R.id.result_view);
         question_1_view = (LinearLayout) findViewById(R.id.question_1_view);
         question_2_view = (LinearLayout) findViewById(R.id.question_2_view);
         question_3_view = (LinearLayout) findViewById(R.id.question_3_view);
-        //question_4_view = (LinearLayout) findViewById(R.id.question_4_view);
+        question_4_view = (LinearLayout) findViewById(R.id.question_4_view);
+        question_5_view = (LinearLayout) findViewById(R.id.question_5_view);
+
+        // Catch the name entered .......
 
         EditText user_name = (EditText) findViewById(R.id.name);
         userName = user_name.getText().toString().toUpperCase();
+
+        // check if name is not entered... if entered goto question 1 view
 
         if(userName.equals("")){
             Toast.makeText(this, getString(R.string.no_name), Toast.LENGTH_SHORT).show();
@@ -59,7 +68,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void gotoQuestion_2(View view){
+    /*
+        ................. Question 1 check and update result or failed and message and move to the next question
+     */
+
+    public void question_1_check(View view){
         RadioGroup group_option = (RadioGroup) findViewById(R.id.question_1_group);
         int user_choice = group_option.getCheckedRadioButtonId();
 
@@ -84,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
         }else{
 
-            // Question is not answered to display a toast....
+            // Question is not answered so display a toast....
 
             Toast.makeText(this, "Answer the question pleas", Toast.LENGTH_SHORT).show();
         }
@@ -92,7 +105,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void gotoQuestion_3(View view){
+    /*
+        ................. Question 2 check and update result or failed and message and move to the next question
+     */
+
+    public void question_2_check(View view){
         RadioGroup group_option = (RadioGroup) findViewById(R.id.question_2_group);
         int user_choice = group_option.getCheckedRadioButtonId();
 
@@ -105,12 +122,14 @@ public class MainActivity extends AppCompatActivity {
             RadioButton option_c = (RadioButton) findViewById(R.id.question_2_c);
             boolean question_2_c = option_c.isChecked();
 
-            if (question_2_c){
+            if (question_2_c){ //answer is correct so update result
                 result += 1;
                 message += getString(R.string.message_info_2) + " \n";
-            }else{
+            }else{ // answer is false
                 failed += "Question 2 \n";
             }
+
+            // Hide view 2 and move to view 3
 
             question_2_view.setVisibility(View.GONE);
             question_3_view.setVisibility(View.VISIBLE);
@@ -123,7 +142,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void gotoQuestion_4(View view){
+    /*
+        ................. Question 3 check and update result or failed and message and move to the next question
+     */
+
+    public void question_3_check(View view){
+
+        // ...... Get all the checkboxes state
+
         CheckBox option_a = (CheckBox) findViewById(R.id.question_3_a);
         boolean question_3_a = option_a.isChecked();
 
@@ -136,7 +162,12 @@ public class MainActivity extends AppCompatActivity {
         CheckBox option_d = (CheckBox) findViewById(R.id.question_3_d);
         boolean question_3_d = option_d.isChecked();
 
+        //........ Check if any checkbox is marked
+
         if(question_3_a || question_3_b || question_3_c || question_3_d){
+
+            // ....... Check if the correct once are marked...
+
             if(question_3_a && !question_3_b && question_3_c && question_3_d){
                 result += 1;
                 message += getString(R.string.message_info_3) + " \n";
@@ -145,13 +176,88 @@ public class MainActivity extends AppCompatActivity {
             }
 
             question_3_view.setVisibility(View.GONE);
+            question_4_view.setVisibility(View.VISIBLE);
 
-            show_result();
-            result_view.setVisibility(View.VISIBLE);
         }else{
+
+            // Question not answered .........
+
             Toast.makeText(this, "Answer the question pleas", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    /*
+        ................. Question 4 check and update result or failed and message and move to the next question
+     */
+
+    public void question_4_check(View view){
+        RadioGroup group_option = (RadioGroup) findViewById(R.id.question_4_group);
+        int user_choice = group_option.getCheckedRadioButtonId();
+
+        // Check if question is answered before moving to the next page
+
+        if(user_choice != -1) {
+
+            // questions is answered so check if answer is correct or not
+
+            RadioButton option_d = (RadioButton) findViewById(R.id.question_4_d);
+            boolean question_4_d = option_d.isChecked();
+
+            if (question_4_d){
+                result += 1;
+                message += getString(R.string.message_info_4) + " \n";
+            }else{
+                failed += "Question 4 \n";
+            }
+
+            question_4_view.setVisibility(View.GONE);
+            question_5_view.setVisibility(View.VISIBLE);
+
+        }else{
+
+            // Question is not answered to display a toast....
+
+            Toast.makeText(this, "Answer the question pleas", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+    .............. Question 5 check begins here........
+     */
+
+    public void question_5_check(View view){
+
+        // Get the text from the edit text view
+
+        EditText get_answer = (EditText) findViewById(R.id.question_5_answer);
+        String question_5_answere = get_answer.toString().toUpperCase();
+
+        // check if user ansered the question or not ........
+
+        if(question_5_answere.equals("")){
+
+            // User did not andwere the question ....
+
+            Toast.makeText(this, "Answer the question pleas", Toast.LENGTH_SHORT).show();
+        }else{
+
+            // Question was answered so lets check the answere with the real answere
+
+            if(question_5_answere.equals(getString(R.string.question_5_a))){
+
+                // Answer is correct
+                result += 1;
+                message += getString(R.string.message_info_5) + " \n";
+            }else{
+                failed += "Question 5 \n";
+            }
+
+            question_5_view.setVisibility(View.GONE);
+
+            show_result();
+            result_view.setVisibility(View.VISIBLE);
+        }
     }
 
     public void reset(View view){
